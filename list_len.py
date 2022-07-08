2ff63c8d9ba2faeec035e7e68dfa09358287238a
shravanimula <98573934+shravanimula@users.noreply.github.com>
2022-07-08 22:08:12 +0530

    list

---
+++ list_len.py
@@ -0,0 +1,15 @@
+string=['hello','thunder soft','india','hyd']
+print(string)
+print(type(string))
+print(len(string))
+list1=[10,20,30,40,50,60]
+print(list1)
+print(type(list1))
+print(len(list1))
+list2=[True,False,False,True]
+print(list2)
+print(len(list2))
+list3=[10,True,'hello',False,'thundersoft',20]
+print(list3)
+print(len(list3))
+
