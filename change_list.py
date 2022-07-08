8db9f69c601d31f9014ad76f9b6cd8ad057fff99
shravanimula <98573934+shravanimula@users.noreply.github.com>
2022-07-08 22:04:16 +0530

    list

---
+++ change_list.py
@@ -0,0 +1,18 @@
+string=['hello','world','thundersoft','india','privated','limited','hyd']
+print(string)
+print(string[1])
+print(string[-1])
+string[1]='good morning'
+print(string)
+string1=['hello','world','os','c','data structur','python']
+print(string1)
+string1[1:3]='hiiii'
+print(string1)
+string2=["hiiii","world","good","morning","hyd"]
+print(string2)
+string2[1:3]=["HELLO"]
+print(string2)
+string3=['hello','thundersoft','india','hyd']
+print(string3)
+string3[1:2]=['hiiii','good morning']
+print(string3)
