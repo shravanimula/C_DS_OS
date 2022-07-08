f253869b8c70621d48cb3f54ce2edf9f902ea530
shravanimula <98573934+shravanimula@users.noreply.github.com>
2022-07-08 22:05:45 +0530

    list_program

---
+++ check_list1.py
@@ -0,0 +1,8 @@
+string=["hello","thundersoft",'india','good morning','thundersoft','india']
+print(string)
+print(type(string))
+print(string[1])
+print(string[3])
+print(string[:7])
+print(string[-3:])
+print(string[:-1])
