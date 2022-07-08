793089c1cef39e99d099819e2ac38085dcd5a38f
shravanimula <98573934+shravanimula@users.noreply.github.com>
2022-07-08 22:08:42 +0530

    list

---
+++ remove.py
@@ -0,0 +1,21 @@
+st=['aaaaa','bbbbb','cccccc','ddddd']
+print(st)
+st.remove('bbbbb')
+print(st)
+st1=['AA','BB','CC','DD']
+print(st1)
+st1.pop(2)
+print(st1)
+st2=['aaa','bbbb','ccc','dddd']
+print(st2)
+st2.pop()
+print(st2)
+st3=['AAA','BBBB','CCCC','DDDD']
+print(st3)
+del st3[0]
+print(st3)
+st4=['aaaaa','qqqq','wwww','eeee','rrrrr']
+print(st4)
+st4.clear()
+print(st4)
+
